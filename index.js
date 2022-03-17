d7175c714b39ff169f228f08e1b64ffda52ac3c7
namo5758 <76212150+namo5758@users.noreply.github.com>
2022-03-17 15:35:26 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const Discord = require("discord.js")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("discord-api-types/v9")
+const fs = require("fs")
+const { Player } = require("discord-player")
+const { token } = require("./config.json")
+
+const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = "721287837898768456"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES"
+    ]
+})
+
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./commands/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(token)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationCommands(CLIENT_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
+else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+        client.user.setStatus('dnd')
+        client.user.setActivity("หี", { type: "WATCHING" })
+    })
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand() {
+            if (!interaction.isCommand()) return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+    client.login(token)
+}
